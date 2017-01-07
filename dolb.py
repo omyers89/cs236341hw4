4776eeb3541e30de5f0d757ebc920d80e487e40c
Myers-Omri <omyers@qti.qualcomm.com>
2017-01-07 19:30:42 +0200

    second try with smart shit

--- dolb.py
+++ dolb.py
@@ -8,14 +8,11 @@ import threading
 
 
 class ServerThread(threading.Thread):
-    #def __init__(self, threadID, name, client_conn, server_conn,data):
     def __init__(self, client_socket, server_socket, data):
         threading.Thread.__init__(self)
-        #self.threadID = threadID
         self.client_socket = client_socket
         self.server_socket = server_socket
         self.data = data
-        #self.name = name
 
     def run(self):
         self.server_socket.send(self.data)
@@ -23,8 +20,6 @@ class ServerThread(threading.Thread):
         self.client_socket.send(response)
         self.client_socket.close()
         return
-
-
 
 
 
@@ -42,90 +37,58 @@ class LoadBlancer():
         self.server_jobs = {}
         for s in self.servers:
             self.server_jobs[s] = 0
-
         self.last_rec_time = 0
-        
         self.weight_db = {'V' : [1,1,3], 'P' : [1,1,2], 'M': [2,2,1]}
 
         
     def run_lb(self):
-
         for s,i in self.servers.items():
             new_server_connection = socket(SK.AF_INET, SK.SOCK_STREAM)
             new_server_connection.connect((i, self.port))
             print " connected to: " + i
             self.connections[s] = new_server_connection
-
         self.client_socket = socket(SK.AF_INET, SK.SOCK_STREAM)
         self.client_socket.bind((self.ip, self.port))
         self.client_socket.listen(5)
 
         while True:
             new_client_socket, client_ip = self.client_socket.accept()
-            #print to log
-
             request = new_client_socket.recv(1024)
-
             print "new mesage from: "+ client_ip[0]
             print "    message is: "  + request
-
-            # media_type = request[0]
-            # proccess_time = int(request[1])
-            best_server = self.pick_server(request) #TODO: routing algorithm return socket obj of selected server
-
+            best_server = self.pick_server(request)
             t = ServerThread(new_client_socket, best_server, request)
             self.active_threads.append(t)
             t.start()
 
-        #for s, sock in self.connections.items():
-        #    sock.close()
-        #    if new_client_socket != None:
-        #        new_client_socket.close()
-
 
     def get_correct_time(self, rec):
         c = rec[0]
-        t = rec[1]
+        t = int(rec[1])
         real_weights = {0: self.server_jobs[0] + t*self.weight_db[c][0],
                         1: self.server_jobs[1] + t*self.weight_db[c][1],
                         2: self.server_jobs[2] + t*self.weight_db[c][2]}
         return real_weights
-        
 
 
-
-    
     def smart_rout(self, rec):
         new_t = time.clock()
         time_delta = new_t - self.last_rec_time
-
-        
         for s,t  in self.server_jobs.items():
             new_jobs = max (t - time_delta,0)
             self.server_jobs[s] = new_jobs
 
-
         temp_jobs = get_correct_time(rec)
-        
         min = sys.maxint
         best = 0
         for s,t in temp_jobs.items():
             if t < min:
                 min = t
                 best = s
-        
+
         self.last_rec_time = new_t
-
         self.server_jobs[best] = min
-
         return best
-        #real_job_time = get_correct_time(rec,0)
-        #best_sev = 0
-        #min_jobs = self.server_jobs[best_sev]    
-        #real_job_time = get_correct_time(rec,s)
-
-
-        
 
 
 
@@ -147,9 +110,7 @@ class LoadBlancer():
 
 
 if __name__ == '__main__':
-    
     server_dict = {0:'192.168.0.101', 1:'192.168.0.102', 2:'192.168.0.103'}
     lb_IP = '10.0.0.1'
-    lb = LoadBlancer(lb_IP, 80,server_dict)
-
+    lb = LoadBlancer(lb_IP, 80,server_dict, 1)
     lb.run_lb()
